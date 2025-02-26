4b87663a7eb3f6def645b0e4c688626e7cc70ec2
Naomiel <naomieloor@gmail.com>
2025-02-26 01:54:44 +0000

    My Second Commit

--- src/main/java/com/qucoon/hospitalmanagement/model/response/PatientResponse.java
+++ src/main/java/com/qucoon/hospitalmanagement/model/response/PatientResponse.java
@@ -3,17 +3,15 @@ package com.qucoon.hospitalmanagement.model.response;
 import com.qucoon.hospitalmanagement.enums.Gender;
 import lombok.Data;
 
-
 @Data
 public class PatientResponse {
-
-        private int id;
-        private String firstName;
-        private String lastName;
-        private String email;
-        private int age;
-        private String phoneNumber;
-        private Gender gender;
-        private String address;
-        private String emergencyContact;
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int age;
+    private String phoneNumber;
+    private Gender gender;
+    private String address;
+    private String emergencyContact;
 }
