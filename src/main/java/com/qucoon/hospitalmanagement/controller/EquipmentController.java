92a2abf2c147b4616823aa58d96df239894a26d8
Naomiel <naomieloor@gmail.com>
2025-03-10 21:07:08 +0000

    Refactored Patient and Appointment response code and the controllers

--- src/main/java/com/qucoon/hospitalmanagement/controller/EquipmentController.java
+++ src/main/java/com/qucoon/hospitalmanagement/controller/EquipmentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/equipment")
+@RequestMapping("api/v1/equipment")
 public class EquipmentController {
     private final EquipmentService equipmentService;
 
